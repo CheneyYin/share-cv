406fdf289a8c39b26a801b69020b0741bbddbcc2
CheneyYin <cheneyyin@hotmail.com>
2024-09-22 16:07:08 +0800

    Add GUN V3 Header

--- src/traffic_analysis.py
+++ src/traffic_analysis.py
@@ -1,3 +1,23 @@
+# 
+# This file is part of the share-cv distribution (https://github.com/CheneyYin/share-cv).
+# Copyright (c) 2024 Chengyu Yan.
+# 
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+# Demo Video: https://www.bilibili.com/video/BV1sVv8euESQ/
+#
 from ultralytics import YOLO, solutions
 import cv2
 import os
